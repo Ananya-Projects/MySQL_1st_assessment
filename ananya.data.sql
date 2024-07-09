1e937e83c90c70765696f0fa4bb1a8ab06c57d0d
Ananya Gupta <ananyagupta.bca23@satyug.edu.in>
2024-07-09 19:23:43 +0530

    ananya.data.sql

--- ananya.data.sql
+++ ananya.data.sql
@@ -1,19 +1,28 @@
 USE ananya;
+
 SELECT * FROM ananya.data;
-SELECT SUM(purch_amt) AS total_purchase_amount
+
+SELECT SUM(purch_amt) AS tpa
 FROM ananya.data;
-SELECT avg(purch_amt) AS average_purchase_amount
+
+SELECT avg(purch_amt) AS apa
 FROM ananya.data;
-SELECT COUNT(DISTINCT salesman_id) AS number_of_salespeople
+
+SELECT COUNT(DISTINCT salesman_id) AS no_of_salespeople
 FROM ananya.data;
-SELECT COUNT(customer_id) AS number_of_customers
+
+SELECT COUNT(customer_id) AS no_of_customer
 FROM ananya.data;
-SELECT MAX(purch_amt) AS maximum_purchase_amount
+
+SELECT MAX(purch_amt) AS max_pa
 FROM ananya.data;
-SELECT MIN(purch_amt) AS minimum_purchase_amount
+
+SELECT MIN(purch_amt) AS min_pa
 FROM ananya.data;
+
 SELECT COUNT(DISTINCT customer_id) AS customers_with_grades
 FROM ananya.data
+
 WHERE grade IS NOT NULL;
 
 
